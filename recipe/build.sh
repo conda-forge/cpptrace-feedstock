ca6cd90817112f499f5d067497ad7b56f0261d19
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-02-27 00:01:07 -0600

    Libunwind support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-cmake ${CMAKE_ARGS} -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=On -DCPPTRACE_GET_SYMBOLS_WITH_LIBDWARF=On -DCPPTRACE_USE_EXTERNAL_LIBDWARF=On -DCPPTRACE_FIND_LIBDWARF_WITH_PKGCONFIG=On -DCMAKE_PREFIX_PATH=$PREFIX
+echo "#########################################################################"
+echo "Building ${PKG_NAME} using unwind back-end $unwind_backend"
+echo "#########################################################################"
+
+export CPPTRACE_UNWIND_WITH_LIBUNWIND=OFF
+if [ "$unwind_backend" == "libunwind" ]; then
+    export CPPTRACE_UNWIND_WITH_LIBUNWIND=ON
+fi
+
+cmake ${CMAKE_ARGS} -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=On -DCPPTRACE_GET_SYMBOLS_WITH_LIBDWARF=On -DCPPTRACE_USE_EXTERNAL_LIBDWARF=On -DCPPTRACE_FIND_LIBDWARF_WITH_PKGCONFIG=On -DCPPTRACE_UNWIND_WITH_LIBUNWIND=$CPPTRACE_UNWIND_WITH_LIBUNWIND -DCMAKE_PREFIX_PATH=$PREFIX
 cmake --build build --config Release --target install
--- recipe/test/main.cpp
+++ recipe/test/main.cpp
@@ -1,7 +1,28 @@
 #include <cpptrace/cpptrace.hpp>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 
 void trace() {
     cpptrace::generate_trace().print();
+    #ifdef TEST_LIBUNWIND
+    if(!cpptrace::can_signal_safe_unwind()) {
+        std::cerr<<"Test failed: !cpptrace::can_signal_safe_unwind()"<<std::endl;
+        abort();
+    }
+    cpptrace::frame_ptr buffer[10];
+    std::size_t count = cpptrace::safe_generate_raw_trace(buffer, 10);
+    if(count == 0) {
+        std::cerr<<"Test failed: cpptrace::safe_generate_raw_trace found no frames"<<std::endl;
+        abort();
+    }
+    cpptrace::safe_object_frame frame;
+    cpptrace::get_safe_object_frame(buffer[0], &frame);
+    if(frame.address_relative_to_object_start == 0 && strlen(frame.object_path) == 0) {
+        std::cerr<<"Test failed: cpptrace::get_safe_object_frame didn't resolve (_dl_find_object likely isn't available)"<<std::endl;
+        abort();
+    }
+    #endif
 }
 
 void foo(int) {
